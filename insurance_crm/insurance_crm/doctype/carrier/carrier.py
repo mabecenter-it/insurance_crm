3b98ac931f6e31d195e69940a6442646ae2f6066
juanpm99 <juanp99@mabecenter.org>
2026-05-21 10:15:30 -0500

    Crear DocType Carrier

---
+++ insurance_crm/insurance_crm/doctype/carrier/carrier.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2026, IT Mabegroup and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Carrier(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		rating_tqhk: DF.Rating
+	# end: auto-generated types
+
+	pass
