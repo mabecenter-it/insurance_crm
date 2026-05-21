23f63ef7b82f4b9ca7f38714f257025d1bb4fe79
Santiago moncada <santiagom01@mabecenter.org>
2026-05-21 09:42:36 -0500

    Aca enrollment module and doctypes

---
+++ insurance_crm/insurance_crm/doctype/aca_enrollment/aca_enrollment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, IT Mabegroup and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ACA Enrollment", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ insurance_crm/insurance_crm/doctype/aca_enrollment/aca_enrollment.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2026, IT Mabegroup and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ACAEnrollment(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		aptc_eligible: DF.Check
+		effective_date: DF.Date | None
+		household_size: DF.Int
+		income: DF.Currency
+		lead: DF.Link | None
+		policy_status: DF.Literal["", "Sin Digitar", "Pending Documents", "Submitted", "Approved", "Active", "Cancelled", "Expired", "Renewal Pending"]
+		state: DF.Literal["", "AZ", "CA", "FL", "GA", "IL", "IN", "LA", "MA", "NC", "NE", "NJ", "OH", "SC", "TN", "TX", "WI"]
+		taxes: DF.Literal["", "Single", "Married Filing Jointly", "Married Filing Separately", "Head of Household"]
+	# end: auto-generated types
+
+	pass
