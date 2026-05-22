56640e16bded94551af8bdf4084967623454db85
Santiago moncada <santiagom01@mabecenter.org>
2026-05-22 11:10:04 -0500

    Se corrige nombre de ACA Enrollment a orden de venta, se organizan campos de numeros telefonicos en contacto crm

--- insurance_crm/insurance_crm/doctype/orden_de_venta/test_orden_de_venta.py
+++ insurance_crm/insurance_crm/doctype/orden_de_venta/test_orden_de_venta.py
@@ -13,9 +13,9 @@ IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 
 
 
-class IntegrationTestACAEnrollment(IntegrationTestCase):
+class IntegrationTestOrdendeVenta(IntegrationTestCase):
 	"""
-	Integration tests for ACAEnrollment.
+	Integration tests for OrdendeVenta.
 	Use this class for testing interactions between multiple components.
 	"""
 
