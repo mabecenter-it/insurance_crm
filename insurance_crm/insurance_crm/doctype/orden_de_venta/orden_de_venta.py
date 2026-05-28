16d1f80b9ea705adc223f3b83f32f14bc4614b5e
Santiago moncada <santiagom01@mabecenter.org>
2026-05-28 14:36:14 -0500

    Revert "Merge pull request #36 from mabecenter-it/feat/Deal-and-OV-connection"
    This reverts commit 9ad3b4b195ce0969cfa0bdbad0c6acd61ed88f30, reversing
    changes made to 2ddfaf3649d04830ee9afdb48bd831df0385915c.

--- insurance_crm/insurance_crm/doctype/orden_de_venta/orden_de_venta.py
+++ insurance_crm/insurance_crm/doctype/orden_de_venta/orden_de_venta.py
@@ -12,9 +12,17 @@ class OrdendeVenta(Document):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
+		from frappe.contacts.doctype.contact_email.contact_email import ContactEmail
+		from frappe.contacts.doctype.contact_phone.contact_phone import ContactPhone
 		from frappe.types import DF
 
+		contacto: DF.Link | None
+		correos_de_contacto: DF.Table[ContactEmail]
+		estado_migratorio: DF.Literal[None]
+		fecha_de_nacimiento: DF.Date | None
 		naming_series: DF.Literal["ACA-.YYYY.-.#####"]
+		nombre_completo: DF.Data | None
+		numeros_de_contacto: DF.Table[ContactPhone]
 	# end: auto-generated types
 
 	pass
