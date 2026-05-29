4e8c42e581785e0d941a524bbf70cf7ddbbcb5d4
juanpm99 <juanp99@mabecenter.org>
2026-05-29 12:55:51 -0500

    "Organizacion de campos de Banco"

--- insurance_crm/insurance_crm/doctype/banco/banco.py
+++ insurance_crm/insurance_crm/doctype/banco/banco.py
@@ -5,7 +5,7 @@
 from frappe.model.document import Document
 
 
-class DatosBancarios(Document):
+class Banco(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
